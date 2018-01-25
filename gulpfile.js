ac6d18c7d3836fd2c6b0d11dde4750eaea4ef3d4
FabianPortillo1996 <Fabianportillo97@gmail.com>
2018-01-24 21:10:58 -0500

    Agregada calculadora

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
-var browsersync= require('browser-sync');
+var browsersync = require('browser-sync');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function () {
+gulp.task('default',['css'], function () {
     browsersync.init({
         server: "./app"
     });
     gulp.watch("app/js/*.js").on('change', browsersync.reload);
+    gulp.watch("scss/**/*.scss", ['css']);
     gulp.watch("app/*.html").on('change', browsersync.reload);
+});
+
+gulp.task('css', function () {
+    return gulp.src('scss/main.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: true
+        }))
+        .pipe(gulp.dest('app/css/'))
+        .pipe(browsersync.stream());
 });

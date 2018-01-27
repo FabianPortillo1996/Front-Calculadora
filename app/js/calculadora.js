14b265f1ad2dee47eff90ef8cb3cf7f28d80b607
FabianPortillo1996 <Fabianportillo97@gmail.com>
2018-01-26 22:54:31 -0500

    Agregada funcionalidad limpiar ( C )

--- app/js/calculadora.js
+++ app/js/calculadora.js
@@ -1,3 +1,21 @@
-function matematicas (operadores, numeros) {
-    
+$('document').ready(function () {
+    var pantalla = $('.pantalla');
+    var contenidoPantalla = pantalla.text();
+    $('.boton').on("click", function () {
+        var btnclick = $(this).text();
+        contenidoPantalla += '' + btnclick;
+        if ('=' === btnclick) {
+            contenidoPantalla = contenidoPantalla.slice(0, -1);
+            operar(contenidoPantalla);
+        }
+        if ('C' === btnclick) {
+            pantalla.html('');
+            contenidoPantalla = "";
+        }
+        pantalla.html(contenidoPantalla);
+    });
+});
+
+function operar(contenidoPantalla) {
+    console.log(contenidoPantalla);
 }
